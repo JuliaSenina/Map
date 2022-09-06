931bdb82afc148f47ff003ecd4a962e6ab8be37f
Юлия Сенина <JuLKa9449@yandex.ru>
2022-09-06 14:45:19 +0300

    commit

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,12 @@
+import ErrorRepository from "../index";
+
+test.each([
+  [ 101, "Переменная не объявлена" ],
+  [ 202, "Некорректное имя" ],
+  [ 303, "Ошибка доступа" ],
+  [ 404, "Unknown error" ],
+])
+("Проверка ошибок", (code, description) => {
+    expect(new ErrorRepository().translate(code)).toBe(description);
+  },
+);
